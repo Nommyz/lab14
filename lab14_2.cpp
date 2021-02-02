c1f0be17ac8662c833cb4dbb159ae5481d01905c
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-02-02 18:22:21 +0700

    mission completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -6,8 +6,10 @@ int main(){
 	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
+	sort(&data[0],&data[5]);
 	cout << "\nAfter: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
+	for(int i = 0; i<5; i++) cout << data[i] << " ";
+	sort(&data[5],&data[10]);
+	for(int i = 5; i<10; i++) cout << data[i] << " ";
 	return 0;
 }
