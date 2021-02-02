572b81696341e7cb5f3a8fde3ad8c50ff8af6fa0
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-02-02 18:21:54 +0700

    Add files via upload

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-	
-	
+int main()
+{
+    int a=5;
+    char b ='A';
+    char &c=b;
+    int *x=&a;
+    char *y=&c;
+    int **z=&x;
+    cout << a <<" "<< b <<" "<< c <<" "<< x <<" "<< &y << " "<< z<< "\n";
+    cout << x <<" "<< &y <<" "<< &y <<" "<< x <<" "<< &y << " "<< z<< "\n";
+	c = 'F';
+	cout << a <<" "<< b <<" "<< c <<" "<< x <<" "<< &y << " "<< z<< "\n";
+	*y ='W';
+	cout << a <<" "<< b <<" "<< c <<" "<< x <<" "<< &y << " "<< z<< "\n";
+	*x=6;
+	cout << a <<" "<< b <<" "<< c <<" "<< x <<" "<< &y << " "<< z<< "\n";
+	**z=7;
+	cout << a <<" "<< b <<" "<< c <<" "<< x <<" "<< &y << " "<< z<< "\n";
 	return 0;
 }
